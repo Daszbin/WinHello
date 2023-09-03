d5eda6cfe564fcc3d8722a469b1e724362d23bc0
Daszbin <37669537+Daszbin@users.noreply.github.com>
2023-09-03 18:57:24 -0400

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinHello")]
+[assembly: AssemblyDescription("Windows Hello")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinHello")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4a484280-0d24-4e7a-912b-cb82136d28ad")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: EdgePasswordExample.Program
+// Assembly: WinHello, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: ADA752EE-764C-4D23-AD8C-D5EF0665FA8D
+// Assembly location: C:\Users\buize\OneDrive\Documents\Codernut-master\CoderNut-master\src\bin\WinHello.exe
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Security.Credentials;
+
+namespace EdgePasswordExample
+{
+  public class Program
+  {
+    private static void Main(string[] args)
+    {
+      if (Program.IsProcessRunning("Coder Nut"))
+        Console.WriteLine(Program.WindowsHelloAsync().Result.ToString());
+      else
+        Application.Exit();
+    }
+
+    private static bool IsProcessRunning(string processName) => Process.GetProcessesByName(processName).Length != 0;
+
+    public static async Task<bool> WindowsHelloAsync() => await KeyCredentialManager.IsSupportedAsync() && (await KeyCredentialManager.RequestCreateAsync("login", (KeyCredentialCreationOption) 0)).Status == 0;
+  }
+}
